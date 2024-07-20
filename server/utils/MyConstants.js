8ac02c15ecd723ac994d440f44fb232bb01ca4b8
Khanhlinh542002 <127309123+Khanhlinh542002@users.noreply.github.com>
2024-07-20 18:46:44 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clusterlwebnc.aefryvs.mongodb.net',
+    DB_USER: 'linhvu',
+    DB_PASS: '123456linh',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'linhvu',
+    JWT_EXPIRES: '3600000000', // in milliseconds
+    EMAIL_USER: 'linhvu5422@gmail.com', // gmail service
+    EMAIL_PASS: 'fgxy flld ierx etnt'
+  };
+  module.exports = MyConstants;
